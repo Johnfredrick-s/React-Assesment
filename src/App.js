439b01974a7045994372389b908a8731dbfd9893
John fredrick <99396744+Johnfredric@users.noreply.github.com>
2024-11-27 09:55:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense, useState } from 'react';
+import Rodal from 'rodal';
+
+// include styles
+import 'rodal/lib/rodal.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const AddSegment = lazy(() => import('./Components/AddSegment'))
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
+  const hide = (data) => {
+    setVisible(false);
+    if (data) {
+      getSegments(data)
+    }
+  }
+
+  const getSegments = async (params) => {
+    const url = "https://webhook.site/660c8134-20bf-4f0b-85b3-e9885291c28e"
+    const response = await fetch(url,{method:'POST',headers:{'Content-Type':'Application/json'},body:JSON.stringify(params)});
+    const data = await response.json();
+    console.log(data)
+    if(data.status == "Success" && data.message){
+      toast.success(data.message)
+    }else{
+      toast.error(data.message)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ToastContainer position='bottom-right' autoClose={2000}  />
+      {visible && <Rodal visible={visible} animation="slideRight" onClose={()=> hide('')} width={300} height={200}>
+        <Suspense fallback={'Loading'}>
+          <AddSegment hide={hide} heading={'Saving segment'} />
+        </Suspense>
+      </Rodal>}
+
+      <main className="main_width py-5">
+        <div>
+          <button onClick={()=> setVisible(true)} className="save_segment">Save segment</button>
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/Components/AddSegment.js
@@ -0,0 +1,138 @@
+import React, { useMemo, useState } from 'react'
+import { toast } from 'react-toastify'
+
+const AddSegment = ({ hide, heading }) => {
+    const [selectedSegment, setSelectedSegment] = useState({})
+    const [segment, setSegment] = useState('')
+    const [submitted, setSubmitted] = useState(false)
+    const [options, setOptions] = useState([
+        // { label: 'Add schema to segment', value: 'Add schema to segment' },
+        { label: 'First Name', value: 'first_name' },
+        { label: 'Last Name', value: 'last_name' },
+        { label: 'Gender', value: 'gender' },
+        { label: 'Age', value: 'age' },
+        { label: 'Account Name', value: 'account_name' },
+        { label: 'City', value: 'city' },
+        { label: 'State', value: 'state' },
+    ])
+
+    const SelectSegment = (e) => {
+        console.log(e, "e")
+
+    }
+
+    const addNewSchema = () => {
+        let schema_segment = document.getElementById('schema-segment')
+        // console.log(schema_segment, "schema_segment")
+        let obj = {}
+        let schema = schema_segment.value
+
+        if (schema) {
+            let key_name = schema.split(",")
+            obj[key_name[0]] = key_name[1]
+            setSelectedSegment(prev => { return { ...prev, ...obj } })
+            schema_segment.value = ''
+        } else {
+            toast.error('Please select a schema to add to segment')
+        }
+    }
+
+    const save_segment = () => {
+        setSubmitted(true)
+        if (Object.keys(selectedSegment).length > 0 && segment) {
+            const payload = {
+                segment_name: segment,
+                schema: [selectedSegment]
+            }
+            hide(payload)
+            // console.log(selectedSegment, "selectedSegment")
+        }
+    }
+
+    const blueBoxOptions = (selectedval) => {
+
+        // Object.keys(selectedSegment).map(val => {
+        return options
+            .filter((option) => {
+                // If it's the first value, include all options.
+                // Otherwise, filter based on the selectedval or any condition.
+                return !Object.keys(selectedSegment).includes(option.value) || option.value == selectedval;
+                // return option.value !== val && option.value == selectedval;
+            })
+            .map((res) => (
+                <option key={res.value} value={res.value + "," + res.label}>
+                    {res.label}
+                </option>
+            ))
+        // })
+
+
+    }
+    return (
+        <>
+            <header className='popup_head'>
+                <div className='popup_head-parent'>
+                    <img className='popup_head-parent-img' onClick={() => hide('')} src='/back-btn.jpg' />
+                    <h1 className='popup_head-parent-title'>{heading}</h1>
+                </div>
+            </header>
+
+            <div className='popup_body'>
+                <div className='popup_body-section'>
+                    <h6 className='popup_body-section-label'>Enter the Name of the Segment</h6>
+                    <input type='text' onChange={(e) => setSegment(e.target.value)} name='segement_name' autoComplete='off' className='popup_body-section-input' />
+                    {(submitted && !segment) && <p className='error-msg'>Segment name is required</p>}
+                </div>
+
+                <h6 className='popup_body-section-label'>To save your segment, you need to add the schemas to build the query</h6>
+
+                <div className='popup_body-blue-box'>
+                    {selectedSegment && Object.keys(selectedSegment).length > 0 ? <>
+                        {Object.keys(selectedSegment).map((selectedval) => {
+                            return (
+                                <div key={selectedval} className="selectedValue">
+                                    <select
+                                        className="select-option"
+                                        placeholder="Add schema to segment"
+                                        value={selectedval + "," + selectedSegment[selectedval]}
+                                        onChange={(e) => SelectSegment(e, selectedval)}
+                                    >
+                                        {blueBoxOptions(selectedval)}
+                                    </select>
+                                </div>
+                            );
+                        })}
+
+
+                    </> : <>No Segment found!</>}
+                </div>
+
+
+                <div className=''>
+                    <select className='select-option' id='schema-segment' placeholder="Add schema to segment" >
+                        <option value="">Add schema to segment</option>
+                        {options.filter(val => {
+                            return !Object.keys(selectedSegment).includes(val.value)
+                        })
+                            .map(res => (
+                                <option key={res.value} value={res.value + "," + res.label}>{res.label}</option>
+                            ))}
+                    </select>
+                </div>
+
+
+                <p className='add_new' onClick={addNewSchema}> + <span className=''>Add new schema</span></p>
+
+            </div>
+
+            <div className='popup_footer'>
+                <div className='popup_footer-btns'>
+                    <button className={`popup_footer-btns-save ${(Object.keys(selectedSegment).length > 0 && segment) ? '' : 'disabled-btn'}`} disabled={Object.keys(selectedSegment).length == 0 && !segment} onClick={save_segment}>Save the segment</button>
+                    <button className='popup_footer-btns-cancel' onClick={() => hide('')}>Cancel</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AddSegment
